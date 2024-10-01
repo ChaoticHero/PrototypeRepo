da7d2e561e44404189fc423c3b369282d1fa7607
ChaoticHero <123209040+ChaoticHero@users.noreply.github.com>
2024-10-01 08:59:14 -0700

    timer

---
+++ ChampionsOfDestiny/Assets/Scripts/TimerScript.cs
@@ -0,0 +1,44 @@
+﻿﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerScript : MonoBehaviour
+{
+    public float TimeLeft;
+    public bool TimerOn = false;
+
+    public Text TimerTxt;
+   
+    void Start()
+    {
+        TimerOn = true;
+    }
+
+    void Update()
+    {
+        if(TimerOn)
+        {
+            if(TimeLeft > 0)
+            {
+                TimeLeft -= Time.deltaTime;
+                updateTimer(TimeLeft);
+            }
+            else
+            {
+                Debug.Log("Time is UP!");
+                TimeLeft = 0;
+                TimerOn = false;
+            }
+        }
+    }
+
+    void updateTimer(float currentTime)
+    {
+        currentTime += 1;
+
+        float minutes = Mathf.FloorToInt(currentTime / 60);
+        float seconds = Mathf.FloorToInt(currentTime % 61);
+
+        TimerTxt.text = string.Format("{00}", seconds);
+    }
+
+}
